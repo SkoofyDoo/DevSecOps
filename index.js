011c2947c38bfeab279c4e075a0ce309c08bc26e
EVGENY KVEST <evgenykvest@gmail.com>
2026-01-01 20:06:24 +0100

    Strukture

---
+++ index.js
@@ -0,0 +1,9 @@
+app.get('/health', (req, res) => {
+    if (!res) {
+        res.status(500).send('Unhealthy');
+    } else {
+        res.status(200).send('Healthy');
+    }
+});
+
+module.exports = app;
